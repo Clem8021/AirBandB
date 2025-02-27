ad59f92cb5227352b7ad53499321d284cdc24762
Ludovico CAMPONOVO <ludovico.camponovo@hotmail.fr>
2025-02-27 22:18:53 +0100

    booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :experience
   belongs_to :user
+
+  validates :date_start, presence: true
+  validates :date_end, presence: true
+  validates :final_price, presence: true
+  validates :status, presence: true
 end
