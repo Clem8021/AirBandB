e6f9a859e04d0d7c972e5b71b3e1fe005ac92632
Clemence Marotte <clemence.marotte@gmail.com>
2025-03-02 16:16:25 +0100

    adding action show

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,7 @@
 class Experience < ApplicationRecord
-  belongs_to :user
+  has_one_attached :photo
+
+  belongs_to :user, optional: true
 
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
